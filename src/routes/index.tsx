e0e1bdd48a01c175eddf7b57f992abd6161aee95
Tumenjargal Altanginj <tumenjargal@pink.kiwi>
2024-10-22 23:33:51 -0500

    expense add, edit modal and list added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Expenses from "../pages/expenses";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/expenses" element={<Expenses />} />
       </Routes>
     </BrowserRouter>
   );
