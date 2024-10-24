7488df255684ea6c0fe073a1f2ea326ee5ef720b
Abdul Moiz <moiza0799@gmail.com>
2024-10-23 20:34:53 -0500

    Code changes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Expenses from "../pages/Expense";
+import Group from "../pages/Home/Group";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Check" element={<Group />} />
         <Route path="/expense/:id" element={<Expenses />} />
       </Routes>
     </BrowserRouter>
